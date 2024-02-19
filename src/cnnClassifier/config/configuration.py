da1d0f5c8d9b29e6f0ca4a335fc8f9f9e851a027
spandanr <spandan.it48@gmail.com>
2024-02-19 21:44:25 +0530

    Commit Evaluation

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,8 +1,9 @@
 from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml, create_directories
+from cnnClassifier.utils.common import read_yaml, create_directories,save_json
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 import os 
 
 class ConfigurationManager:
@@ -70,5 +71,16 @@ class ConfigurationManager:
         params_image_size=params.IMAGE_SIZE
         )
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+        path_of_model="artifacts/training/model.h5",
+        training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+        mlflow_uri="https://dagshub.com/spandanr/DL_DVC_MLFlow.mlflow",
+        all_params=self.params,
+        params_image_size=self.params.IMAGE_SIZE,
+        params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
     
     
